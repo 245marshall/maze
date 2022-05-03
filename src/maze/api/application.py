13790e7e074e68eebaf8d356c2b69d1bc82614e4
Marshall Markham <mmarkham@aidentified.com>
2022-05-03 09:22:14 -0400

    add CORS middleware to api application allowing react to contact API

--- src/maze/api/application.py
+++ src/maze/api/application.py
@@ -2,6 +2,7 @@
 from typing import Literal
 from typing import List
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel as BaseModel
 
 from maze.generation.algorithm import Maze as _Maze
@@ -10,8 +11,16 @@ from maze.generation.algorithm import Algorithms as _Algorithms
 
 MAX_ROW_COLUMN_PRODUCT = 1000
 
-Algorithm = Literal["binary_search", "aldus_broder", "sidewinder"]
+Algorithm = Literal[
+    "binary_search",
+    "aldus_broder",
+    "sidewinder"
+]
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost",
+]
 
 class CellOutput(BaseModel):
     row: int
@@ -28,17 +37,24 @@ class MazeOutput(BaseModel):
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/maze/{algorithm}/", response_model=MazeOutput)
+@app.get("/maze/{algorithm}", response_model=MazeOutput)
 def read_maze_algorithm(algorithm: Algorithm, rows: int, columns: int) -> MazeOutput:
     if rows * columns > MAX_ROW_COLUMN_PRODUCT:
         raise ValueError()
     maze = _Maze(rows, columns)
     data = maze.generate_maze(_Algorithms[algorithm.upper()])
-
     return data
